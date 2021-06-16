e1161e509f41c424389e996b99f193977e51bcf6
Chris Baum <Christopher.Baum@valpo.edu>
2021-06-15 21:03:53 -0400

    setting up fish model relationships

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,3 +1,6 @@
 class Fish < ActiveRecord::Base
-
+  has_many :catches
+  has_many :users, through: :catches
+  has_many :locations, through: :catches
+  has_many :baits, through: :catches
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :catches
   has_many :fish, through: :catches
   has_many :locations, through: :catches
-  has_many :bait, through: :catches
+  has_many :baits, through: :catches
 
 end
