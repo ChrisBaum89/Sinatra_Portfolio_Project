fbe7130a08b0ce800753716006f68d22ec94a89b
Chris Baum <Christopher.Baum@valpo.edu>
2021-07-13 20:10:00 -0400

    added UserController logic for singup, login, logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,43 @@
 class UserController < ApplicationController
 
+  get "/signup" do
+      if Helpers.is_logged_in?(session)
+        redirect '/catches'
+      else
+        erb :"users/signup"
+      end
+    end
+
+    post "/signup" do
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect '/catches'
+      else
+        redirect '/signup'
+      end
+    end
+
+    get "/login" do
+      if Helpers.is_logged_in?(session)
+        redirect '/catches'
+      else
+        erb :"users/login"
+      end
+    end
+
+    post '/login' do
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/catches'
+      else
+        redirect '/login'
+      end
+    end
+
+    get "/logout" do
+      session.clear
+      redirect '/login'
+    end
 end
