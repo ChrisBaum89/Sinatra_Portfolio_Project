ff6c3eef017d16225cb28f68879df6431b77a613
Chris Baum <Christopher.Baum@valpo.edu>
2021-07-13 20:14:23 -0400

    added logic to catch controller

--- app/controllers/catch_controller.rb
+++ app/controllers/catch_controller.rb
@@ -1,3 +1,20 @@
 class CatchController < ApplicationController
-  
+  get "/catch/new" do
+    if Helpers.is_logged_in?(session)
+      erb :"catch/new"
+    else
+      redirect '/login'
+    end
+  end
+
+  get "/catch/:id" do
+    if Helpers.is_logged_in?(session)
+      @catch = Catch.find_by_id(params[:id])
+      @catch_creator = User.find_by_id(@catch.user_id)
+      erb :"catch/show"
+    else
+      redirect '/login'
+    end
+  end
+
 end
