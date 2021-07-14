da7bbb90c0a3197319bb06ac2fa3e4ab54969151
Chris Baum <Christopher.Baum@valpo.edu>
2021-07-13 21:01:25 -0400

    added bait/index control to view index page added clarification notes to intent of each page

--- app/controllers/bait_controller.rb
+++ app/controllers/bait_controller.rb
@@ -1,3 +1,16 @@
 class BaitController < ApplicationController
 
+  get "/bait" do
+    erb :"bait/index"
+  end
+
+  get "/bait/new" do
+      erb :"bait/new"
+  end
+
+  get "/bait/:id" do
+      @bait = Bait.find_by_id(params[:id])
+      @bait_creator = User.find_by_id(@bait.user_id)
+      erb :"bait/show"
+  end
 end
