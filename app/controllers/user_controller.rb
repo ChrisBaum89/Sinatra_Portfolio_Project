2d62bc45b0b16ad4d632f466127d29c8bf2881ac
Chris Baum <Christopher.Baum@valpo.edu>
2021-07-15 19:04:28 -0400

    User model - added has_secure_password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,6 +9,7 @@ class UserController < ApplicationController
     end
 
     post "/signup" do
+      binding.pry
       @user = User.new(username: params[:username], email: params[:email], password: params[:password])
       if @user.save
         session[:user_id] = @user.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :baits
   has_many :catches, through: :baits
   has_many :fish, through: :catches
+
+  validates_presence_of :username, :email, :password
+
 end
