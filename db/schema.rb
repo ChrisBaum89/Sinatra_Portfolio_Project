c9845ca300941f0d4071225eac6e4d7a7e946843
Chris Baum <Christopher.Baum@valpo.edu>
2021-07-15 18:52:44 -0400

    added email to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_224954) do
+ActiveRecord::Schema.define(version: 2021_07_15_225150) do
 
   create_table "baits", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_224954) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
   end
 
 end
