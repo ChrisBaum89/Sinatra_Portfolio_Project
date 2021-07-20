9797b4f7e19f49e989e1dde64a6d5c3cfc79d9d7
Chris Baum <Christopher.Baum@valpo.edu>
2021-07-19 22:16:04 -0400

    added editing of baits

--- app/controllers/bait_controller.rb
+++ app/controllers/bait_controller.rb
@@ -21,9 +21,32 @@ class BaitController < ApplicationController
       erb :"baits/show"
   end
 
+  get "/baits/:id/edit" do
+    @bait = Bait.find_by_id(params[:id])
+    if @bait && (Helpers.current_user(session).id == @bait.user_id)
+      erb :"baits/edit"
+    elsif Helpers.is_logged_in?(session)
+      redirect '/baits'
+    else
+      redirect '/login'
+    end
+  end
+
   post '/baits' do
     @bait = Bait.new(name: params["bait_name"], color: params["bait_color"], user_id: session[:user_id])
     @bait.save
     redirect :"baits/#{@bait.id}"
+  end
+
+  patch '/baits/:id' do
+    @bait = Bait.find_by_id(params[:bait_id])
+    if (params[:bait_name] != "") && (params[:bait_color] != "")
+      @bait.name = params[:bait_name]
+      @bait.color= params[:bait_color]
+      @bait.save
+      redirect "/baits/#{@bait.id}"
+    else
+      redirect "/baits/#{@bait.id}/edit"
+    end
   end
 end
