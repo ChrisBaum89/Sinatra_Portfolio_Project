e3ccb767f393f8809b1fddf67421d8c947ac4220
Chris Baum <Christopher.Baum@valpo.edu>
2021-07-20 19:44:05 -0400

    added self.fish_valid, self.bait_valid, self.new_bait methods

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,4 +6,17 @@ class Helpers
   def self.is_logged_in?(session)
     !!session[:user_id]
   end
+
+  def self.fish_valid(params)
+    (params[:fish_species] != "") && (params[:fish_weight] != "") && (params[:fish_length] != "")
+  end
+
+  def self.bait_valid(params)
+    ((params[:bait_name] != "") && (params[:bait_color] != "") || (params[:bait_id_checked] != nil))
+  end
+
+  def self.new_bait(params)
+    @bait = Bait.new(name: params["bait_name"], color: params["bait_color"], user_id: session[:user_id])
+    @bait.save
+  end
 end
