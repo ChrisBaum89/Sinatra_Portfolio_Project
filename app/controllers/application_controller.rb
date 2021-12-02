73d1fa8755c2d2a35e1dbb06699c24de5c793c22
Chris Baum <Christopher.Baum@valpo.edu>
2021-12-01 21:40:00 -0600

    added securerandom and modified session_secret to read value from a file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'securerandom'
 
 class ApplicationController < Sinatra::Base
 
@@ -6,7 +7,10 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "secret"
+    #session_secret is set by value in config/session_secret.md file.
+    #value in that file was randomly generated with SecureRandom.hex(64)
+    #session_secret.md file is ignored by .gitignore to protect session_secret
+    set :session_secret, File.read('config/session_secret.md')
   end
 
   get "/" do
